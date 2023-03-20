70d5003c6df17b30f324a70814a07443925743c7
Mighty-i <kotniya@gmail.com>
2023-03-20 20:25:20 +0700

    EX2

---
+++ phattharadanai_app_dev/src/Ex2/Ex2Generator.java
@@ -0,0 +1,198 @@
+package Ex2;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+import javax.swing.*;
+
+public class Ex2Generator extends JPanel implements ActionListener {
+
+	static final String BOUND_STR = "bound";
+	static final String NUMBER_STR = "number";
+	static final String BARWIDTH_STR = "bar width";
+	static final String GRAPHWIDTH_STR = "graph width";
+	static final String GENERATE_STR = "Generate";
+
+	Random random;
+	Ex2MyBarGraph barGraph;
+	Ex2MyLineGraph lineGraph;
+
+	JTextField boundTextField;
+	JTextField numberTextField;
+	JTextField barWidthTextField;
+	JTextField graphWidthTextField;
+
+	JButton genButton;
+
+	static int intRadioRow = 10;
+	static int doubleRadioRow = 50;
+
+	JTabbedPane tabbedPane;
+
+	static int boundTextCol = 120;
+	static int boundLabelCol = 220;
+	static int barWidthTextCol = 350;
+	static int barWidthLabelCol = 450;
+	static int genButtonCol = 600;
+
+	int number = 0;
+	int bound = 0;
+	int bw = 0;
+	int gw = 0;
+
+	static int width = 1200;
+	static int height = 900;
+
+	Ex2MyBarGraph myBG;
+	Ex2MyLineGraph myLG;
+
+	public Ex2Generator() {
+
+		setLayout(null);
+		setPreferredSize(new Dimension(width, height));
+		setLayout(null);
+
+		boundTextField = new JTextField(7);
+		boundTextField.setBounds(107, 74, 62,
+				20);
+		add(boundTextField);
+		JLabel boundLabel = new JLabel("Bound");
+		boundLabel.setBounds(120, 61, 49,
+				14);
+		add(boundLabel);
+
+		numberTextField = new JTextField(7);
+		numberTextField.setBounds(205, 75, 62,
+				20);
+		add(numberTextField);
+		JLabel numberLabel = new JLabel("Number");
+		numberLabel.setBounds(218, 61, 55,
+				14);
+		add(numberLabel);
+
+		barWidthTextField = new JTextField(7);
+		barWidthTextField.setBounds(307, 74, 62,
+				20);
+		add(barWidthTextField);
+		JLabel barWidthLabel = new JLabel("Bar width");
+		barWidthLabel.setBounds(313, 61, 62,
+				14);
+		add(barWidthLabel);
+
+		graphWidthTextField = new JTextField(7);
+		graphWidthTextField.setBounds(410, 74, 62,
+				20);
+		add(graphWidthTextField);
+
+		JLabel graphWidthLabel = new JLabel("Graph width");
+		graphWidthLabel.setBounds(410, 61, 77,
+				14);
+		add(graphWidthLabel);
+
+		random = new Random(System.currentTimeMillis());
+
+		genButton = new JButton(GENERATE_STR);
+		genButton.setBounds(522, 73, 127,
+				23);
+		add(genButton);
+		genButton.addActionListener(this);
+
+		tabbedPane = new JTabbedPane();
+		tabbedPane.setSize(1180, 762);
+		tabbedPane.setLocation(10, 127);
+		add(tabbedPane);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+		System.out.println("command:" + command);
+		if (command.equals(GENERATE_STR)) {
+			String strBound = boundTextField.getText();
+			String strNumber = numberTextField.getText();
+
+			String strBW = barWidthTextField.getText();
+			String strGW = graphWidthTextField.getText();
+			try {
+				bound = Integer.parseInt(strBound);
+			} catch (NumberFormatException ex) {
+				bound = 0;
+			}
+			try {
+				number = Integer.parseInt(strNumber);
+			} catch (NumberFormatException ex) {
+				number = 0;
+			}
+			System.out.println("bound: " + bound + ", number: " + number);
+			if (number == 0) {
+				return;
+			} else if (bound == 0) {
+				return;
+			}
+
+			System.out.println("....Generating....");
+			int[] array;
+
+			array = genInt(number, bound);
+
+			for (int i = 0; i < array.length; i++) {
+				System.out.print(array[i] + ",");
+			}
+
+			System.out.println();
+
+			tabbedPane.removeAll();
+
+			myLG = new Ex2MyLineGraph(bound, array);
+			tabbedPane.addTab("Line Graph", myLG);
+
+			bw = 40;
+			gw = 600;
+			if (strBW.isEmpty() == false) {
+				bw = Integer.parseInt(strBW);
+			}
+			if (strGW.isEmpty() == false) {
+				gw = Integer.parseInt(strGW);
+			}
+			myBG = new Ex2MyBarGraph(bound, array, bw, gw);
+			tabbedPane.addTab("Bar Graph", myBG);
+
+		}
+
+	}
+
+	int[] genInt(int number, int bound) {
+
+		int[] figures = new int[number];
+		for (int i = 0; i < number; i++) {
+			int figure = random.nextInt(bound);
+			figures[i] = figure;
+		}
+
+		return figures;
+
+	}
+
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("Ex2");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JComponent newContentPane = new Ex2Generator();
+		newContentPane.setOpaque(true); // <-- content panes must be opaque
+		frame.setContentPane(newContentPane);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+}
---
+++ phattharadanai_app_dev/src/Ex2/Ex2MyLineGraph.java
@@ -0,0 +1,166 @@
+package Ex2;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Ex2MyLineGraph extends JPanel {
+	
+	final static int PAD_RIGHT = 50;
+	final static int PAD_LEFT = 100;
+	final static int PAD_TOP = 50;
+	final static int PAD_BOTTOM = 60;
+	final static int OFFSET_TOP = 30;
+	final static int OFFSET_BOTTOM = 20;
+	final static int OFFSET_X_AXIS = PAD_LEFT - 40;
+	final static int OFFSET_Y_AXIS = PAD_BOTTOM - 10;
+	final static int SCALE_LENGTH = 8;
+	
+	String graphLabel = "Line Graph";
+	String xLabel = "Data Axis";
+	String yLabel = "Value Axis";
+	
+	int width;
+	int height;
+	int graphWidth;
+	int graphHeight;
+	
+	int fontHeight = 10;
+		
+	int dataBound;
+	int[] data;
+	
+	double valueRatio;
+
+	public Ex2MyLineGraph(int dataBound, int[] data) {
+		
+		this(800, 600, dataBound, data);
+		
+	}
+	
+	public Ex2MyLineGraph(int w, int h, int dataBound, int[] data) {
+		
+		super();
+		
+		this.dataBound = dataBound;
+		this.data = data;
+
+		width = w;
+		height = h;
+		
+		setPreferredSize(new Dimension(width, height));
+		
+		graphWidth = width - PAD_RIGHT - PAD_LEFT;
+		graphHeight = height - PAD_TOP - PAD_BOTTOM;
+		
+		valueRatio = graphHeight*1.0/dataBound;
+		
+		System.out.println("xAxisWidth:" + graphWidth
+				+ ", yAxisHeight:" + graphHeight
+				+ ", valueRatio:" + valueRatio);
+		
+	}
+	
+	public int[] scaleData(int[] raw) {
+		int[] scaled = new int[raw.length];
+		
+		int i = 0;
+		for(int data : raw) {
+			
+			double temp = valueRatio * data;
+			int dataScaled = (int)Math.round(temp);
+			scaled[i++] = dataScaled;
+			System.out.println("raw:" + data + ", temp:" + temp 
+					+ ", dataScaled:" + dataScaled);
+		}
+		
+		return scaled;
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+		
+		int[] scaledData = scaleData(data);
+		int yBlockSize = graphHeight / (scaledData.length+1);
+		int xBlockSize = graphWidth / (scaledData.length-1);
+		int dataBlockSize = dataBound/(data.length+1);
+		System.out.println("yBlockSize:" + yBlockSize + ", xBlockSize:" + xBlockSize + ", dataBlockSize:" + dataBlockSize);
+		
+		int x1, y1, x2, y2;
+		
+		// graph label
+		x1 = PAD_LEFT + (graphWidth/2);
+		y1 = PAD_TOP - OFFSET_TOP;
+		g2.drawString(graphLabel, x1, y1);
+		
+		// draw x label
+		x1 = PAD_LEFT + (graphWidth/2);
+		y1 = height - OFFSET_BOTTOM;
+		g2.drawString(xLabel, x1, y1);
+		
+		// draw x axis
+		x1 = PAD_LEFT;
+		y1 = height - PAD_BOTTOM;
+		x2 = width - PAD_RIGHT;		
+		y2 = y1;
+		g2.drawLine(x1, y1, x2, y2);
+
+		// draw scale on x axis
+		x1 = PAD_LEFT;
+		y1 = height - PAD_BOTTOM;
+		y2 = y1 + SCALE_LENGTH;
+		for(int i=0; i<scaledData.length; i++) {
+			x2 = x1;
+			g2.drawLine(x1, y1, x2, y2);
+			g2.drawString("" + i, x1, (height-OFFSET_Y_AXIS) + fontHeight);
+			// move right one block
+			x1 += xBlockSize;
+		}
+				
+		// draw y label
+		x1 = PAD_LEFT;
+		y1 = OFFSET_TOP;
+		g2.drawString(yLabel, x1, y1);
+
+		// draw y axis		
+		x1 = PAD_LEFT;
+		y1 = PAD_TOP;		
+		x2 = PAD_LEFT;
+		y2 = height - PAD_BOTTOM;
+		g2.drawLine(x1, y1, x2, y2);
+
+		// draw scale on y axis
+		x1 = PAD_LEFT;
+		x2 = PAD_LEFT - SCALE_LENGTH;
+		y1 = height - PAD_BOTTOM;
+		for(int i=0; i<=scaledData.length+1; i++) {
+			y2 = y1;
+			g2.drawLine(x1, y1, x2, y2);
+			g2.drawString(String.valueOf(dataBlockSize*i), OFFSET_X_AXIS, y1);
+			// move up one block
+			y1 -= yBlockSize;
+		}
+				
+		// draw graph
+		x1 = PAD_LEFT;
+		for(int i=0; i<data.length-1; i++) {
+			
+			y1 = height-PAD_BOTTOM - scaledData[i];
+			x2 = x1 + xBlockSize;
+			y2 = height-PAD_BOTTOM - scaledData[i+1];
+			g2.drawString(String.valueOf(data[i]), x1, y1);
+			if(i == scaledData.length-1) {
+				g2.drawString(String.valueOf(scaledData[i+1]), x2, y2);
+			}
+			
+			// draw graph between a pair of point
+			g2.drawLine(x1, y1, x2, y2);
+			x1 += xBlockSize;
+			
+		}
+		
+	}
+	
+
+
+}
